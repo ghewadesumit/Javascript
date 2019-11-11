7eb9acee83a97250abcdb0b0a1dcd4893bbef680
Sumit <ghewdesumit@gmail.com>
2019-11-11 08:30:23 -0800

    remove node_modules folder

---
+++ after.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var greet = function greet(name) {
+  console.log('Greetings ', name);
+};
+
+greet('Sumoz');
---
+++ before.js
@@ -0,0 +1,6 @@
+const greet = (name) => {
+
+    console.log('Greetings ',name);
+}
+
+greet('Sumoz');
