503e3437b63c77925484a09fb77d9709d55efc88
Sumit <ghewdesumit@gmail.com>
2019-11-12 19:34:02 -0800

    project ui started

---
+++ ProjectUI_Library/src/index.js
@@ -0,0 +1,6 @@
+import ToolTip from './webapp/tooltips';
+
+const tooltip = new ToolTip(document.querySelector('.tooltip'));
+console.log('I\'m ready to build projects');
+
+tooltip.init();
---
+++ ProjectUI_Library/src/webapp/tooltips.js
@@ -0,0 +1,24 @@
+import './styles/tooltip.css';
+class ToolTip{
+    constructor(element){
+        this.element = element;
+        this.message = element.getAttribute('data-message');
+    }
+    
+    init(){
+        const tip = document.createElement('div');
+        tip.classList.add('tip');
+        tip.textContent = this.message;
+        this.element.appendChild(tip);
+
+        this.element.addEventListener('mouseenter',()=>{
+            tip.classList.add('active')
+        })
+        
+        this.element.addEventListener('mouseleave',()=>{
+            tip.classList.remove('active');
+        })
+    }
+}
+
+export {ToolTip as default};
