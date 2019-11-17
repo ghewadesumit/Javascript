42fda0657a5441d006fb482a73b2f5d82564ac3c
Sumit <ghewdesumit@gmail.com>
2019-11-16 18:36:05 -0800

    completed ProjectUI-library

--- ProjectUI_Library/src/index.js
+++ ProjectUI_Library/src/index.js
@@ -1,6 +1,31 @@
 import ToolTip from './webapp/tooltips';
+import Dropdown from './webapp/dropdown';
+import Tabs from './webapp/tabs';
+import Snackbar from './webapp/snackbar';
+
 
 const tooltip = new ToolTip(document.querySelector('.tooltip'));
 console.log('I\'m ready to build projects');
 
 tooltip.init();
+  
+const dropdowns = document.querySelectorAll('.dropdown');
+console.log(typeof(dropdowns));
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+})
+
+
+//create Tabs
+
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+//show snackbar
+const snack = new Snackbar();
+    snack.init();
+document.querySelector('.snackbar-trigger').addEventListener('click',()=>{
+
+    snack.show();
+})
